d8b8c652a6a961afedfbfee6d0f00b1c74fc2450
sdonaxistracking <sdonaxistracking@gmail.com>
2013-08-27 22:03:34 -0500

    fuck this

---
+++ OnAxisTracking/Debug/src/test.d
@@ -0,0 +1,6 @@
+src/test.d src/test.o: ../src/test.c /usr/local/include/opencv/cv.h \
+  /usr/local/include/opencv/highgui.h
+
+/usr/local/include/opencv/cv.h:
+
+/usr/local/include/opencv/highgui.h:
