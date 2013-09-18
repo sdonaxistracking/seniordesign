b2511c2816923d0dc30d2b9917a4f3bc4e4fde4b
sdonaxistracking <sdonaxistracking@gmail.com>
2013-09-17 19:01:36 -0500

    WebCam Testing

--- OnAxisTracking/src/test.c
+++ OnAxisTracking/src/test.c
@@ -27,48 +27,22 @@
 #include <highgui.h>
 
 
-int main()
-{
-  IplImage* img = 0; 
-  int height,width,step,channels;
-  uchar *data;
-  int i,j,k;
+int main(int argc, char** argv){
+	cvNamedWindow("Camera", CV_WINDOW_AUTOSIZE);
+	CvCapture* capture = cvCaptureFromCAM(-1);
+	IplImage* frame;
+	IplImage* zoomImage;
+	while(1){
+		frame = cvQueryFrame(capture);
+		zoomImage = cvCreateImage(cvSize(frame->width * 2, frame->height*2), frame->depth, frame->nChannels);
+		cvResize(frame, zoomImage,CV_INTER_LINEAR);
 
- /* if(argc<2){
-    printf("Usage: main <image-file-name>\n\7");
-    exit(0);
-  }
-*/
-  // load an image  
- /* img=cvLoadImage(argv[1]);
-  if(!img){
-    printf("Could not load image file: %s\n",argv[1]);
-    exit(0);
-  }
-*/
-  // get the image data
-  height    = img->height;
-  width     = img->width;
-  step      = img->widthStep;
-  channels  = img->nChannels;
-  data      = (uchar *)img->imageData;
-  printf("Processing a %dx%d image with %d channels\n",height,width,channels); 
+		if(!frame)break;
 
-  // create a window
-  cvNamedWindow("mainWin", CV_WINDOW_AUTOSIZE); 
-  cvMoveWindow("mainWin", 100, 100);
-
-  // invert the image
-  for(i=0;i<height;i++) for(j=0;j<width;j++) for(k=0;k<channels;k++)
-    data[i*step+j*channels+k]=255-data[i*step+j*channels+k];
-
-  // show the image
-  cvShowImage("mainWin", img );
-
-  // wait for a key
-  cvWaitKey(0);
-
-  // release the image
-  cvReleaseImage(&img );
-  return 0;
+		cvShowImage("Camera", zoomImage);
+		char c = cvWaitKey(33);
+		if(c == 27) break;
+	}
+	cvReleaseCapture(&capture);
+	cvDestroyWindow("Camera");
 }
